993bb1a35719b50689765b6682fe22d34eb186a1
MantasKITM <73162805+MantasKITM@users.noreply.github.com>
2021-07-07 14:07:05 +0300

    Registracija turi loading indikatorių. Pakeistas FX kintamojo pavadinimas LOGIN kode

--- src/sample/controller/RegisterController.java
+++ src/sample/controller/RegisterController.java
@@ -2,14 +2,13 @@ package sample.controller;
 
 
 import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.mindrot.jbcrypt.BCrypt;
@@ -25,14 +24,24 @@ import java.util.ResourceBundle;
 
 public class RegisterController extends Main implements Initializable {
 
+    @FXML
     public Button register_button;
+    @FXML
     public Label form_info_label;
+    @FXML
     public TextField company_name_textfield;
+    @FXML
     public PasswordField password_passwordfield;
+    @FXML
     public TextField email_textfield;
+    @FXML
     public TextField first_name_textfield;
+    @FXML
     public TextField last_name_textfield;
+    @FXML
     public TextField password_confirm_passwordfield;
+    @FXML
+    public ProgressIndicator load_progress_indicator;
 
 
     private String hashPassword(String plainTextPassword) {
@@ -46,59 +55,88 @@ public class RegisterController extends Main implements Initializable {
 
     //Checks all register fields are correct
     public void register() {
+        register_button.setVisible(false);
+        loadProgress();
+        Thread registerLogicalThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (first_name_textfield.getText().isEmpty()
+                        || last_name_textfield.getText().isEmpty()
+                        || email_textfield.getText().isEmpty()
+                        || company_name_textfield.getText().isEmpty()
+                        || password_passwordfield.getText().isEmpty()
+                        || password_confirm_passwordfield.getText().isEmpty()
+                ) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.CREDENTIALS_IS_NOT_FILLED);
+                    });
+                    return;
+                } else if (!Validation.isValidFirstName(first_name_textfield.getText())) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_FIRST_NAME);
+                    });
+                    return;
+                } else if (!Validation.isValidLastName(last_name_textfield.getText())) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_LAST_NAME);
+                    });
+                    return;
+                } else if (!Validation.isValidEmail(email_textfield.getText())) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.CREDENTIALS_IS_NOT_FILLED_EMAIL);
+                    });
+                    return;
+                } else if (!Validation.isValidCompanyName(company_name_textfield.getText())) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_COMPANY_NAME);
+                    });
+                    return;
+                } else if (!Validation.isValidPassword(password_passwordfield.getText())) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_PASSWORD);
 
-        if (first_name_textfield.getText().isEmpty()
-                || last_name_textfield.getText().isEmpty()
-                || email_textfield.getText().isEmpty()
-                || company_name_textfield.getText().isEmpty()
-                || password_passwordfield.getText().isEmpty()
-                || password_confirm_passwordfield.getText().isEmpty()
-        ) {
-            WarnStyle();
-            form_info_label.setText(Constants.CREDENTIALS_IS_NOT_FILLED);
-            return;
-        } else if (!Validation.isValidFirstName(first_name_textfield.getText())) {
-            WarnStyle();
-            form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_FIRST_NAME);
-            return;
-        } else if (!Validation.isValidLastName(last_name_textfield.getText())) {
-            WarnStyle();
-            form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_LAST_NAME);
-            return;
-        } else if (!Validation.isValidEmail(email_textfield.getText())) {
-            WarnStyle();
-            form_info_label.setText(Constants.CREDENTIALS_IS_NOT_FILLED_EMAIL);
-            return;
-        } else if (!Validation.isValidCompanyName(company_name_textfield.getText())) {
-            WarnStyle();
-            form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_COMPANY_NAME);
-            return;
-        } else if (!Validation.isValidPassword(password_passwordfield.getText())) {
-            WarnStyle();
-            form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_PASSWORD);
-            return;
-        } else if (!Validation.isValidPassword(password_confirm_passwordfield.getText())) {
-            WarnStyle();
-            form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_PASSWORD);
-            return;
-        } else if (!password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
-            WarnStyle();
-            form_info_label.setText(Constants.PASSWORD_IS_NOT_EQUAL);
-        }
-        //Sending data with email address and getting answer is it exists in boolean
-        boolean credentials = UserDAO.compareEmailForValidation(email_textfield.getText());
-
-        //If theres is no same email
-        if (!credentials) {
-            //Creating new user
-            registerToDataBase();
-        } else {
-            form_info_label.setText("");
-            form_info_label.setStyle("-fx-text-fill: red;");
-            form_info_label.setText(Constants.EMAIL_EXISTS);
-            email_textfield.setStyle("-fx-text-fill: red;");
-        }
+                    });
+                    return;
+                } else if (!Validation.isValidPassword(password_confirm_passwordfield.getText())) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.CREDENTIALS_IS_NOT_CORRECT_PASSWORD);
+                    });
+                    return;
+                } else if (!password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
+                    Platform.runLater(() -> {
+                        WarnStyle();
+                        form_info_label.setText(Constants.PASSWORD_IS_NOT_EQUAL);
+                    });
+                }
+                //Sending data with email address and getting answer is it exists in boolean
+                boolean credentials = UserDAO.compareEmailForValidation(email_textfield.getText());
+                Platform.runLater(() -> {
+                    //If theres is no same email
+                    if (!credentials) {
+                        //Creating new user
+                        registerToDataBase();
+                    } else {
+                        form_info_label.setText("");
+                        form_info_label.setStyle("-fx-text-fill: red;");
+                        form_info_label.setText(Constants.EMAIL_EXISTS);
+                        email_textfield.setStyle("-fx-text-fill: red;");
+                        register_button.setVisible(true);
+                        load_progress_indicator.setVisible(false);
+                    }
+                });
+            }
+        });
+        registerLogicalThread.setDaemon(true);
+        registerLogicalThread.start();
     }
+
 
     public void closeRegister() { //Uzdaro prisijungimo langa
         Stage stage = (Stage) register_button.getScene().getWindow();
@@ -190,4 +228,22 @@ public class RegisterController extends Main implements Initializable {
         //back to login screen
         goToLogin();
     }
+
+    private void loadProgress() {
+        Task copyWorker = createWorker();
+        load_progress_indicator.progressProperty().bind(copyWorker.progressProperty());
+        new Thread(copyWorker).start();
+        load_progress_indicator.setVisible(true);
+    }
+
+    public Task createWorker() {
+        return new Task() {
+            @Override
+            protected TabPane call() throws Exception {
+                TabPane tabPane = new TabPane();
+
+                return tabPane;
+            }
+        };
+    }// Loading Spinner set-up-ends
 }
