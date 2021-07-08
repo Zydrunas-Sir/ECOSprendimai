609456d18ddfe4199c1b759b838c6afa24dfc5b6
MantasKITM <73162805+MantasKITM@users.noreply.github.com>
2021-07-08 13:52:39 +0300

    Praplėstas slaptažodžių sutapimo funkcionalumas. Tikrinami abu laukai, kad slaptažodžiai būtų identiški

--- src/sample/controller/RegisterController.java
+++ src/sample/controller/RegisterController.java
@@ -288,17 +288,27 @@ public class RegisterController extends Main implements Initializable {
             } else {
                 form_info_label.setText("");
             }
-        });
-            password_confirm_passwordfield.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!password_confirm_passwordfield.getText().isEmpty()) {
-                    if (!password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
-                        form_info_label.setStyle("-fx-text-fill: black;");
-                        form_info_label.setText("Įvesti slaptažodžiai turi sutapti.");
-                return;
-                    } else if (password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
-                        form_info_label.setText("");
-                    }
+            if (!password_passwordfield.getText().isEmpty()) {
+                if (!password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
+                    form_info_label.setStyle("-fx-text-fill: black;");
+                    form_info_label.setText("Įvesti slaptažodžiai turi sutapti.");
+                    return;
+                } else if (password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
+                    form_info_label.setText("");
                 }
+            }
+        });
+        password_confirm_passwordfield.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!password_confirm_passwordfield.getText().isEmpty()) {
+                if (!password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
+                    form_info_label.setStyle("-fx-text-fill: black;");
+                    form_info_label.setText("Įvesti slaptažodžiai turi sutapti.");
+                    return;
+                } else if (password_passwordfield.getText().equals(password_confirm_passwordfield.getText())) {
+                    form_info_label.setText("");
+                    return;
+                }
+            }
         });
     }
 
